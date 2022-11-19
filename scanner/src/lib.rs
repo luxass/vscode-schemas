9441367226f82594bb0fecfccbd961bc589a1cc8
Lucas Nørgård <lucasnrgaard@gmail.com>
2022-11-19 22:24:39 +0100

    testing agsain

--- scanner/src/lib.rs
+++ scanner/src/lib.rs
@@ -4,7 +4,9 @@ extern crate log;
 #[macro_use]
 extern crate serde;
 
-use std::{fs::File, io::Read};
+use std::fs::{metadata, File};
+use std::io::{Read, Write};
+use walkdir::WalkDir;
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct Metadata {
@@ -17,4 +19,25 @@ pub fn read_metadata() -> Metadata {
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
     serde_json::from_str::<Metadata>(&contents).unwrap()
+}
+
+pub fn scan_for_files(dir: &str) -> Result<Vec<String>, std::io::Error> {
+    let mut files: Vec<String> = Vec::new();
+    for entry in WalkDir::new(dir).into_iter().filter_map(Result::ok) {
+        let path = entry.path();
+
+        let metadata = metadata(&path)?;
+        if metadata.is_file() {
+            match path.extension() {
+                Some(ext) => {
+                    if ext == "json" || ext == "jsonc" || ext == "ts" || ext == "js" {
+                        files.push(path.to_str().unwrap().to_string());
+                    }
+                }
+                None => {}
+            }
+        }
+    }
+
+    Ok(files)
 }
--- scanner/src/main.rs
+++ scanner/src/main.rs
@@ -1,14 +1,15 @@
-use std::{env, io, path::Path};
+use std::{env, fs, io, path::Path};
 
 use anyhow::Result;
 use flate2::read::GzDecoder;
-use log::info;
+use log::{debug, info};
 use octocrab::{
     models::repos::{Object, Ref},
     params::repos::Reference,
     Octocrab,
 };
-use scanner_lib::{read_metadata, Metadata};
+use regex::Regex;
+use scanner_lib::{read_metadata, scan_for_files, Metadata};
 use std::fs::File;
 use std::io::Cursor;
 use tar::Archive;
@@ -27,7 +28,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     info!("metadata version: {:?}", metadata.version);
 
     let extraction_dir: &Path = Path::new("../extraction");
-
+    if !extraction_dir.exists() {
+        info!("Creating extraction directory");
+        std::fs::create_dir(extraction_dir)?;
+    }
     let github_token = env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN not set");
 
     let octocrab = Octocrab::builder().personal_token(github_token).build()?;
@@ -45,18 +49,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     info!("latest release: {}", last_release_tag_name);
     let tag: Reference = Reference::Tag(last_release_tag_name.clone());
-    info!("getting tag: {:?}", tag);
 
     let _ref: Ref = repo.get_ref(&tag).await?;
-    info!("ref: {:?}", _ref.object);
-    let url = if let Object::Tag { url, .. } = _ref.object.clone() {
-        url
-    } else {
-        panic!("not a tag");
-    };
-    info!("url: {}", url);
 
-    let long_sha = if let Object::Commit { sha, url: _ } = _ref.object {
+    let long_sha = if let Object::Tag { sha, url: _ } = _ref.object {
         sha
     } else {
         panic!("not a commit");
@@ -94,6 +90,55 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let tar = GzDecoder::new(tar_gz);
         let mut archive = Archive::new(tar);
         archive.unpack(extraction_dir.join("."))?;
+    }
+
+    // TODO: Read extensions folders here.
+
+    let files = scan_for_files(src_folder.to_str().unwrap())?;
+    info!("found {} files.", files.len());
+
+    let mut schema_paths = Vec::<String>::new();
+
+    let re = Regex::new(r"vscode://schemas(?:/\w+)+").unwrap();
+    for file in files {
+        let contents = fs::read_to_string(&file).expect("failed to read file");
+
+        let lines = contents.lines();
+
+        lines.for_each(|line| {
+            let captures = re.captures(line);
+            if let Some(captures) = captures {
+                let path = captures.get(0).map_or("", |m| m.as_str()).to_string();
+                debug!("{:?} - {:?}", path, &file);
+                schema_paths.push(path);
+            }
+        });
+    }
+
+    schema_paths = schema_paths
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<std::collections::HashSet<_>>()
+        .iter()
+        .map(|x| x.to_string())
+        .collect();
+
+    schema_paths.sort();
+    info!("SCHEMAS = {:?}", schema_paths);
+
+    metadata = Metadata {
+        version: last_release_tag_name,
+        schemas: schema_paths,
+    };
+
+    if Path::new(src_folder.to_str().unwrap()).exists() {
+        fs::remove_dir_all(src_folder.to_str().unwrap()).unwrap();
+    }
+    
+    let tar_gz_file_path = extraction_dir.join(&tar_gz_file);
+    
+    if Path::new(&tar_gz_file_path).exists() {
+        fs::remove_file(&tar_gz_file_path).unwrap();
     }
 
     Ok(())
