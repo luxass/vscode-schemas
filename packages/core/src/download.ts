cb552eb4490f35e1e7fd78599f79e2f490d1fc45
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-06-18 13:56:56 +0200

    hmm

--- packages/core/src/download.ts
+++ packages/core/src/download.ts
@@ -1,3 +1,25 @@
-export async function download(release: string) {
+import { stat } from "node:fs/promises";
+import type { Release } from "./releases";
+
+export type DownloadOptions = {
+  /**
+   * The directory to download the source code to.
+   * @default "vscode-src"
+   */
+  outDir?: string
+};
+
+export async function download(release: Release, {
+  outDir = "vscode-src"
+}: DownloadOptions) {
+
+  if (!outDir) outDir = "vscode-src";
+
+  const outDirStat = await stat(outDir);
+
+  if (outDirStat.isDirectory()) {
+    throw new Error("The output directory already exists, please remove it or specify a different one.");
+  }
+
 
 }
