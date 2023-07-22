c650b630761e3f413cc4aa84fd8512fd571d12bd
luxass <lucasnrgaard@gmail.com>
2023-07-22 19:04:54 +0200

    fix: tasks & launch now works correctly

---
+++ apps/schema-extractor-internal/tsup.config.ts
@@ -0,0 +1,11 @@
+import {
+  defineConfig
+} from "tsup";
+
+export default defineConfig({
+  entry: ["./src/extension.ts"],
+  format: ["cjs"],
+  clean: true,
+  treeshake: true,
+  external: ["vscode"]
+});
